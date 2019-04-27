3df9fb248d95dfb865999b36aaaab6c39809cbec
Dmitry Edrenov <edrenoff@icloud.com>
2019-04-27 10:48:28 +0700

    Initial Commit

---
+++ FotoGCD_Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FotoGCD_Swift
+//
+//  Created by Dmitry on 27/04/2019.
+//  Copyright © 2019 Dmitry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
